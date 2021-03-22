e9d2ba78a8db6c8f6c884ba13226ece15b577cf2
unknown <raselmr005@gmail.com>
2021-03-22 17:47:47 +0600

    init serverless express app

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 
 
-const app = require('./app.js');
+const server = require('./server.js');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+server.listen(3000, () => console.log('Local app listening on port 3000!'));
